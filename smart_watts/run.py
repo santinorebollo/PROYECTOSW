1c5700e779b6064eb6a62c9d6ee380187c7d1648
santinorebollo <santinorebollo110@gmail.com>
2026-08-26 19:00:39 -0300

    Fix indentation for app initialization

--- smart_watts/run.py
+++ smart_watts/run.py
@@ -159,8 +159,8 @@ if __name__ == '__main__':
     """)
     
     # Inicializar la aplicación
-        if config_name == 'development':
-            initialize_app()
+    if config_name == 'development':
+        initialize_app()
     
     # Obtener configuración
     config_name = os.environ.get('FLASK_ENV', 'development')
